68e29ddaa724865868c3cfe920915e4c95de9f1a
Rafael Lucas <103396704+R4f4elucc4s@users.noreply.github.com>
2022-05-14 19:21:25 -0300

    Add files via upload

---
+++ TED.py
@@ -0,0 +1,9 @@
+
+glossário={'Algoritmos':'um conjunto das regras e procedimentos lógicos perfeitamente definidos','Python':'uma linguagem de programação de alto nível',
+'Lógica de Programação':'a técnica de encadear pensamentos para atingir determinado objetivo','Processador':'um hardware que processa informações relacionadas ao computador','Cooler':'hardware que resfria o processador'}
+for i in glossário:
+    
+    print('\n')
+    print(f'{i} é {glossário[i]}')
+
+
